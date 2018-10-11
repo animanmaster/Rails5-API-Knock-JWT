066df477b104fe3d186230b016257d0131d45b29
Malik Ahmed <animanmaster@gmail.com>
2018-10-10 22:50:32 -0400

    Update routes.rb to indicate version v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   root 'home#index'
-  get 'auth' => 'home#auth'
-  post 'user_token' => 'user_token#create'
+  get '/v1/auth' => 'home#auth'
+  post '/v1/user_token' => 'user_token#create'
 
-  get    '/users'          => 'users#index'
-  get    '/users/current'  => 'users#current'
-  post   '/users/create'   => 'users#create'
-  patch  '/user/:id'       => 'users#update'
-  delete '/user/:id'       => 'users#destroy'
+  get    '/v1/users'          => 'users#index'
+  get    '/v1/users/current'  => 'users#current'
+  post   '/v1/users/create'   => 'users#create'
+  patch  '/v1/user/:id'       => 'users#update'
+  delete '/v1/user/:id'       => 'users#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
